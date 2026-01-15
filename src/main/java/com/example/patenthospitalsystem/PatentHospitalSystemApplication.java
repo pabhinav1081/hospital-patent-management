ade3ca3a14ebff65f49b73b32a561eea6c53c892
abhinav11 <abhinavpandey2405@gmail.com>
2026-01-15 18:56:04 +0530

    start project

---
+++ src/main/java/com/example/patenthospitalsystem/PatentHospitalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.patenthospitalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatentHospitalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatentHospitalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/patenthospitalsystem/PatentHospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patenthospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatentHospitalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
